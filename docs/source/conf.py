dea2c6f8b4a8c323e3f0ec1b3e5a58c5b97a25dd
BlackCatDevel0per <bcdev@mail.ru>
2022-06-04 19:57:08 +0500

    Update file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,6 @@ extensions = [
 
 pygments_style = "sphinx"
 version = '0.1.0'
-html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -53,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
